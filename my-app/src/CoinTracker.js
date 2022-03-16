8435d5fa3c94f017eb68edf61edfb1e79c087c21
안예진 <ahw24033@naver.com>
2022-03-16 17:09:50 +0900

    외부에 있는 데이터를 가지고 왔어요!

---
+++ my-app/src/CoinTracker.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function CoinTracker() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  });
+
+  return (
+    <div>
+      <h1>The coins!</h1>
+      {loading ? <strong>Loading...</strong> : null}
+    </div>
+  );
+}
+
+export default CoinTracker;
