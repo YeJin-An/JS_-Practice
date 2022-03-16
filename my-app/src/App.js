b6b7699e29c65226befa5f6dfbbfd732306eb702
안예진 <ahw24033@naver.com>
2022-03-16 17:27:00 +0900

    외부에 있는 데이터를 UI로 표현하기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
-import ToDoList from "./ToDoList";
+import CoinTracker from "./CoinTracker";
 
 function App() {
   return (
     <div>
-      <ToDoList />
+      <CoinTracker />
     </div>
   );
 }
--- my-app/src/CoinTracker.js
+++ my-app/src/CoinTracker.js
@@ -2,17 +2,28 @@ import { useEffect, useState } from "react";
 
 function CoinTracker() {
   const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
   });
 
   return (
     <div>
-      <h1>The coins!</h1>
+      <h1>The coins! ({coins.length})</h1>
       {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin) => (
+          <li>
+            {coin.name}({coin.symbol}): {coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
