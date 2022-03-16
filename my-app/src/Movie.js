244f6997cf6a379e9e072fe26c7db14072237f03
안예진 <ahw24033@naver.com>
2022-03-16 18:32:51 +0900

    .then이 아닌 asyc await를 사용한 결과

--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -4,19 +4,51 @@ function Movie() {
   const [loading, setLoading] = useState(true);
   const [movie, setMovie] = useState([]);
 
+  // const getMovies = async () => {
+  //   const response = await fetch(
+  //     "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+  //   );
+  //   const json = await response.json();
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+
+    setMovie(json.data.movies);
+    setLoading(false);
+  };
+
   useEffect(() => {
-    fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovie(json.data.movies);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
+
   console.log(movie);
 
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movie.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Movie;
