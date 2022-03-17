2a5d0352b3aa21f01a9b73958c7481965563e6b0
안예진 <ahw24033@naver.com>
2022-03-17 11:09:28 +0900

    Loading을 멈추는 Link

--- my-app/src/Movie1.js
+++ my-app/src/Movie1.js
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie1({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <a href="/movie">{title}</a>
+        <Link to="/movie">{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
