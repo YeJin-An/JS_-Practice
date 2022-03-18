76c8d821fcdbbd39d8fba3b0486cd47b88a850f7
안예진 <ahw24033@naver.com>
2022-03-18 14:54:49 +0900

    components를 planning 했떠여!

---
+++ movie_app_2022/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    moviee: [],
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false, book: true });
+    }, 6000);
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
+}
+
+export default App;
