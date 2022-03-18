7dc3ce914c41ae314175df4604378c8f0de69a6d
안예진 <ahw24033@naver.com>
2022-03-18 11:17:53 +0900

    props object를 만들었어요!

--- movie_app_2022/src/App.js
+++ movie_app_2022/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ name }) {
+  return <h3>I like {name}</h3>;
+}
 
 function App() {
   return (
     <div>
       <h1>JS-Pracive</h1>
-      <Potato />
+      <Food name="Kimchi" fav={true} papapapa={["hello", 1, 2, 3, 4, true]} />
     </div>
   );
 }
--- movie_app_2022/src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Potato() {
-  return <h3>Potato is my good food</h3>;
-}
-
-export default Potato;
