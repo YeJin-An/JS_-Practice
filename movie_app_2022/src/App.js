2fb9a8c03bb209569b90972e75c2d4ec0e4626e6
안예진 <ahw24033@naver.com>
2022-03-18 13:42:12 +0900

    PropTypes를 작성했어요!

--- movie_app_2022/src/App.js
+++ movie_app_2022/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
+function Food({ name, image, rating }) {
   return (
     <div>
-      <h3>I like {name}</h3>
-      <img src={picture} alt={name} />
+      <h3>
+        I like {name}[{rating}/5.0]
+      </h3>
+      <img src={image} alt={name} />
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 const foodILike = [
   {
@@ -15,29 +24,40 @@ const foodILike = [
     name: "Kimchi",
     image:
       "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating: 4,
   },
   {
     id: 2,
     name: "bulgogi",
     image:
       "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
+    rating: 304,
   },
   {
     id: 3,
     name: "kimbap",
     image:
       "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
+    rating: 3.9,
   },
   {
     id: 4,
     name: "samgyetang",
     image:
       "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 5,
   },
 ];
 
 function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.image} />;
+  return (
+    <Food
+      key={dish.id}
+      name={dish.name}
+      rating={dish.rating}
+      image={dish.image}
+    />
+  );
 }
 
 function App() {
