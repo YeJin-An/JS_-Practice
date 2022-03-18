6d6dcddb3cc2c0a0f3227e284c0ec10975d344cb
안예진 <ahw24033@naver.com>
2022-03-18 15:20:42 +0900

    axios 사용

--- movie_app_2022/src/App.js
+++ movie_app_2022/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
@@ -7,14 +8,14 @@ class App extends React.Component {
     moviee: [],
   };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false, book: true });
-    }, 4000);
+    const axios.get("https://yts.mx/api/v2/list_movies.json");
   }
   render() {
     const { isLoading } = this.state;
     return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
+
+// javascript에서 data가져오는 요것이 fetch 이에여!
 
 export default App;
